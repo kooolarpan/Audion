fc5bb5bd1bf3b6079595492e6a66a9e5a1ffc0fb
Kool Arpan <koolrapperarpan@gmail.com>
2026-02-21 01:09:06 +0545

    fix: package-lock.json

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -31,8 +31,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_os::init());
+        .plugin(tauri_plugin_notification::init());
 
     #[cfg(desktop)]
     {
